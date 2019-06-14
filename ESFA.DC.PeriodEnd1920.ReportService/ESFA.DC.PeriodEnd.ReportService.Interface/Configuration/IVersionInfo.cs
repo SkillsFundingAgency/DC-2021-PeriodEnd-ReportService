ebed4297933fb850a91eea786eaca7311e991bdd
Gayatri <gayatrippatel@hotmail.com>
2019-06-14 11:26:01 +0100

    76485-Initial project structure

---
+++ ESFA.DC.PeriodEnd1920.ReportService/ESFA.DC.PeriodEnd.ReportService.Interface/Configuration/IVersionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Interface.Configuration
+{
+    public interface IVersionInfo
+    {
+        string ServiceReleaseVersion { get; set; }
+    }
+}

2df6dfcf2a8cc36d24316a1ce42f5ad918ca150e
Gayatri <gayatrippatel@hotmail.com>
2019-06-18 16:32:10 +0100

    76577-Integrating with esfa common sf

--- ESFA.DC.PeriodEnd1920.ReportService/ESFA.DC.PeriodEnd.ReportService.Stateless/Program.cs
+++ ESFA.DC.PeriodEnd1920.ReportService/ESFA.DC.PeriodEnd.ReportService.Stateless/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using Autofac;
+using Autofac.Integration.ServiceFabric;
+using ESFA.DC.PeriodEnd.ReportService.Service;
+using ESFA.DC.ServiceFabric.Common.Config;
+using ESFA.DC.ServiceFabric.Common.Config.Interface;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace ESFA.DC.PeriodEnd.ReportService.Stateless
@@ -14,22 +19,30 @@ namespace ESFA.DC.PeriodEnd.ReportService.Stateless
         {
             try
             {
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
+                IServiceFabricConfigurationService serviceFabricConfigurationService = new ServiceFabricConfigurationService();
 
-                ServiceRuntime.RegisterServiceAsync("ESFA.DC.PeriodEnd1920.ReportService.StatelessType",
-                    context => new Stateless(context)).GetAwaiter().GetResult();
+                // Setup Autofac
+                ContainerBuilder builder = DIComposition.BuildContainer(serviceFabricConfigurationService);
 
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(Stateless).Name);
+                // Register the Autofac magic for Service Fabric support.
+                builder.RegisterServiceFabricSupport();
 
-                // Prevents this host process from terminating so services keep running.
-                Thread.Sleep(Timeout.Infinite);
+                // Register the stateless service.
+                builder.RegisterStatelessService<ServiceFabric.Common.Stateless>("ESFA.DC.PeriodEnd1920.ReportService.StatelessType");
+
+                using (var container = builder.Build())
+                {
+                    container.Resolve<EntryPoint>();
+
+                    ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(ServiceFabric.Common.Stateless).Name);
+
+                    // Prevents this host process from terminating so services keep running.
+                    Thread.Sleep(Timeout.Infinite);
+                }
             }
             catch (Exception e)
             {
-                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e + Environment.NewLine + (e.InnerException?.ToString() ?? "No inner exception"));
                 throw;
             }
         }

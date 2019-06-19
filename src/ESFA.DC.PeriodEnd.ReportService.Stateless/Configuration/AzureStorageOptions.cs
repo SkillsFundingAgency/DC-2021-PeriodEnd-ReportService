862d1dcc1a7c546fb813510c289c4fd33ce8301a
Gayatri <gayatrippatel@hotmail.com>
2019-06-19 14:49:02 +0100

    fixing merge

---
+++ src/ESFA.DC.PeriodEnd.ReportService.Stateless/Configuration/AzureStorageOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.PeriodEnd.ReportService.Interface.Configuration;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Stateless.Configuration
+{
+    public class AzureStorageOptions : IAzureStorageOptions
+    {
+        public string AzureBlobConnectionString { get; set; }
+
+        public string AzureBlobContainerName { get; set; }
+    }
+}

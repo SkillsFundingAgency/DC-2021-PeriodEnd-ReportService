443a53caabc098e7d8ea149c3ceb30197cc09176
Gayatri <gayatrippatel@hotmail.com>
2019-06-27 09:09:55 +0100

    77511-code review feedback

--- src/ESFA.DC.PeriodEnd.ReportService.Service/Provider/FM36PeriodEndProviderService.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Service/Provider/FM36PeriodEndProviderService.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using ESFA.DC.ILR1819.DataStore.EF.Interface;
-using ESFA.DC.IO.Interfaces;
 using ESFA.DC.Logging.Interfaces;
 using ESFA.DC.PeriodEnd.ReportService.Interface.Provider;
 using ESFA.DC.PeriodEnd.ReportService.Model.PeriodEnd.AppsMonthlyPayment;
 using ESFA.DC.PeriodEnd.ReportService.Service.Provider.Abstract;
-using ESFA.DC.Serialization.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace ESFA.DC.PeriodEnd.ReportService.Service.Provider
@@ -18,16 +15,11 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Provider
     public class FM36PeriodEndProviderService : AbstractFundModelProviderService, IFM36PeriodEndProviderService
     {
         private readonly Func<IIlr1819RulebaseContext> _ilrRulebaseContextFactory;
-        private readonly SemaphoreSlim _getDataLock = new SemaphoreSlim(1, 1);
-        private bool _loadedDataAlready;
-        private FM36Global _fundingOutputs;
 
         public FM36PeriodEndProviderService(
             ILogger logger,
-            IStreamableKeyValuePersistenceService storage,
-            IJsonSerializationService jsonSerializationService,
             Func<IIlr1819RulebaseContext> ilrRulebaseContextFactory)
-        : base(storage, jsonSerializationService, logger)
+        : base(logger)
         {
             _ilrRulebaseContextFactory = ilrRulebaseContextFactory;
         }

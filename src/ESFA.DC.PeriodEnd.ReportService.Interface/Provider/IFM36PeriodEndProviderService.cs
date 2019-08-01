cd82d7258f97b5ced0f94468d4bd1de18ad85550
Saqib <5689652+saqib2013@users.noreply.github.com>
2019-08-01 14:16:38 +0100

    #84130 - Apps additional report imported into the repository and datastore packages upgraded to 1920 and entity framework packages updated to the ones required to support new 1920 datastore package.

--- src/ESFA.DC.PeriodEnd.ReportService.Interface/Provider/IFM36PeriodEndProviderService.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Interface/Provider/IFM36PeriodEndProviderService.cs
@@ -1,11 +1,14 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using ESFA.DC.PeriodEnd.ReportService.Model.PeriodEnd.AppsAdditionalPayment;
 using ESFA.DC.PeriodEnd.ReportService.Model.PeriodEnd.AppsMonthlyPayment;
 
 namespace ESFA.DC.PeriodEnd.ReportService.Interface.Provider
 {
     public interface IFM36PeriodEndProviderService
     {
+        Task<AppsAdditionalPaymentRulebaseInfo> GetFM36DataForAppsAdditionalPaymentReportAsync(int ukPrn, CancellationToken cancellationToken);
+
         Task<AppsMonthlyPaymentRulebaseInfo> GetFM36DataForAppsMonthlyPaymentReportAsync(int ukPrn, CancellationToken cancellationToken);
     }
 }

ccce45f81c6ea843eb62cc7cc39729e46a400f8b
Gayatri <gayatrippatel@hotmail.com>
2019-08-15 15:58:29 +0100

    85609-basic skeleton of the report

--- src/ESFA.DC.PeriodEnd.ReportService.Interface/ReportTaskNameConstants.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Interface/ReportTaskNameConstants.cs
@@ -8,5 +8,6 @@ namespace ESFA.DC.PeriodEnd.ReportService.Interface
     {
         public const string AppsMonthlyPaymentReport = "TaskGenerateAppsMonthlyPaymentReport";
         public const string AppsAdditionalPaymentsReport = "TaskGenerateAppsAdditionalPaymentsReport";
+        public const string AppsCoInvestmentContributionsReport = "TaskGenerateAppsCoInvestmentContributionsReport";
     }
 }
---
+++ src/ESFA.DC.PeriodEnd.ReportService.Service/Reports/AppsCoInvestmentContributionsReport.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CsvHelper;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.IO.Interfaces;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.PeriodEnd.ReportService.Interface;
+using ESFA.DC.PeriodEnd.ReportService.Interface.Provider;
+using ESFA.DC.PeriodEnd.ReportService.Interface.Reports;
+using ESFA.DC.PeriodEnd.ReportService.Interface.Service;
+using ESFA.DC.PeriodEnd.ReportService.Model.PeriodEnd.AppsCoInvestmentContributions;
+using ESFA.DC.PeriodEnd.ReportService.Service.Mapper;
+using ESFA.DC.PeriodEnd.ReportService.Service.Reports.Abstract;
+using ReportTaskNameConstants = ESFA.DC.PeriodEnd.ReportService.Interface.ReportTaskNameConstants;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports
+{
+    public sealed class AppsCoInvestmentContributionsReport : AbstractReport, IReport
+    {
+        private readonly IIlrPeriodEndProviderService _ilrPeriodEndProviderService;
+        private readonly IDASPaymentsProviderService _dasPaymentsProviderService;
+
+        public AppsCoInvestmentContributionsReport(
+            ILogger logger,
+            IStreamableKeyValuePersistenceService streamableKeyValuePersistenceService,
+            IDateTimeProvider dateTimeProvider,
+            IValueProvider valueProvider,
+            IIlrPeriodEndProviderService ilrPeriodEndProviderService,
+            IDASPaymentsProviderService dasPaymentsProviderService)
+        : base(dateTimeProvider, valueProvider, streamableKeyValuePersistenceService, logger)
+        {
+            _ilrPeriodEndProviderService = ilrPeriodEndProviderService;
+            _dasPaymentsProviderService = dasPaymentsProviderService;
+        }
+
+        public override string ReportFileName => "Apps Co-Investment Contributions Report";
+
+        public override string ReportTaskName => ReportTaskNameConstants.AppsCoInvestmentContributionsReport;
+
+        public override async Task GenerateReport(IReportServiceContext reportServiceContext, ZipArchive archive, bool isFis, CancellationToken cancellationToken)
+        {
+            var externalFileName = GetFilename(reportServiceContext);
+            var fileName = GetZipFilename(reportServiceContext);
+            string csv = await GetCsv(reportServiceContext, cancellationToken);
+            await _streamableKeyValuePersistenceService.SaveAsync($"{externalFileName}.csv", csv, cancellationToken);
+            await WriteZipEntry(archive, $"{fileName}.csv", csv);
+        }
+
+        private async Task<string> GetCsv(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            List<AppsCoInvestmentContributionsModel> appsCoInvestmentContributionsModels = new List<AppsCoInvestmentContributionsModel>();
+
+            using (var ms = new MemoryStream())
+            {
+                UTF8Encoding utF8Encoding = new UTF8Encoding(false, true);
+                using (TextWriter textWriter = new StreamWriter(ms, utF8Encoding))
+                {
+                    using (CsvWriter csvWriter = new CsvWriter(textWriter))
+                    {
+                        WriteCsvRecords<AppsCoInvestmentContributionsMapper, AppsCoInvestmentContributionsModel>(csvWriter, appsCoInvestmentContributionsModels);
+                        csvWriter.Flush();
+                        textWriter.Flush();
+                        return Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}

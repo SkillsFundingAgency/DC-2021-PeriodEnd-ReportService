c70e168f74789ccda07e3d9aaa44d6afdb3f4d24
Mike Childs <mike@mjcdev.co.uk>
2019-10-25 15:48:00 +0100

    Unit Test Fix

--- src/ESFA.DC.PeriodEnd.ReportService.Service.Tests/Reports/TestAppsAdditionalPaymentsReport.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Service.Tests/Reports/TestAppsAdditionalPaymentsReport.cs
@@ -108,9 +108,9 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Tests.Reports
 
             result.Should().NotBeNullOrEmpty();
             result.Count().Should().Be(1);
-            result.First().AugustEarnings.Should().Be(0);
+            result.First().AugustEarnings.Should().Be(20);
             result.First().JulyEarnings.Should().Be(0);
-            result.First().TotalEarnings.Should().Be(0);
+            result.First().TotalEarnings.Should().Be(20);
             result.First().TotalPaymentsYearToDate.Should().Be(20);
             result.First().UniqueLearnerNumber.Should().Be(12345);
             result.First().EmployerNameFromApprenticeshipService.Should().Be(employerNameExpected);

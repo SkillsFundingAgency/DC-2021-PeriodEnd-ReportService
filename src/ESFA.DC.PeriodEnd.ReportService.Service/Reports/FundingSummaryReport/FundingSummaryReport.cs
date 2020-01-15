5ae548d3836511d9fa2d95531a695d899be2da1e
kelvin.morris <kelvin.morris@eas.co.uk>
2020-01-15 14:35:13 +0000

    #92204 persisting FundingSummaryReport data

--- src/ESFA.DC.PeriodEnd.ReportService.Service/Reports/FundingSummaryReport/FundingSummaryReport.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Service/Reports/FundingSummaryReport/FundingSummaryReport.cs
@@ -1,9 +1,13 @@
-﻿using System.IO.Compression;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.DateTimeProvider.Interface;
 using ESFA.DC.IO.Interfaces;
 using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.PeriodEnd.DataPersist;
 using ESFA.DC.PeriodEnd.ReportService.Interface;
 using ESFA.DC.PeriodEnd.ReportService.Interface.Model.FundingSummaryReport;
 using ESFA.DC.PeriodEnd.ReportService.Interface.Provider;
@@ -11,6 +15,7 @@ using ESFA.DC.PeriodEnd.ReportService.Interface.Service;
 using ESFA.DC.PeriodEnd.ReportService.Service.Constants;
 using ESFA.DC.PeriodEnd.ReportService.Service.Interface;
 using ESFA.DC.PeriodEnd.ReportService.Service.Reports.Abstract;
+using ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport.Model;
 
 namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport
 {
@@ -21,6 +26,7 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport
         private readonly IPeriodisedValuesLookupProviderService _periodisedValuesLookupProvider;
         private readonly IFCSProviderService _fcsProviderService;
         private readonly IFundingSummaryReportModelBuilder _modelBuilder;
+        private readonly IPersistReportData _persistReportData;
 
         public FundingSummaryReport(
             ILogger logger,
@@ -30,7 +36,8 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport
             IExcelService excelService,
             IRenderService<IFundingSummaryReport> fundingSummaryReportRenderService,
             IPeriodisedValuesLookupProviderService periodisedValuesLookupProvider,
-            IFCSProviderService fcsProviderService)
+            IFCSProviderService fcsProviderService,
+            IPersistReportData persistReportData)
             : base(
                 dateTimeProvider,
                 streamableKeyValuePersistenceService,
@@ -41,6 +48,7 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport
             _fundingSummaryReportRenderService = fundingSummaryReportRenderService;
             _periodisedValuesLookupProvider = periodisedValuesLookupProvider;
             _fcsProviderService = fcsProviderService;
+            _persistReportData = persistReportData;
         }
 
         public override string ReportFileName => "Funding Summary Report";
@@ -73,6 +81,51 @@ namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport
                 await _excelService.SaveWorkbookAsync(workbook, replacedFileName, reportServiceContext.Container, cancellationToken);
 
                 await WriteZipEntry(archive, $"{fileName}.xlsx", workbook, cancellationToken);
+            }
+
+            if (reportServiceContext.DataPersistFeatureEnabled)
+            {
+                var persistModels = model.FundingCategories.SelectMany(fc => fc.FundingSubCategories.SelectMany(fsc =>
+                    fsc.FundLineGroups.Select(flg => new FundingSummaryPersistModel
+                    {
+                        Ukprn = reportServiceContext.Ukprn,
+                        ContractNo = flg.ContractAllocationNumber,
+                        FundingCategory = fc.FundingCategoryTitle,
+                        FundingSubCategory = fsc.FundingSubCategoryTitle,
+                        FundLineGroup = flg.Title,
+                        Aug19 = flg.Period1,
+                        Sep19 = flg.Period2,
+                        Oct19 = flg.Period3,
+                        Nov19 = flg.Period4,
+                        Dec19 = flg.Period5,
+                        Jan20 = flg.Period6,
+                        Feb20 = flg.Period7,
+                        Mar20 = flg.Period8,
+                        Apr20 = flg.Period9,
+                        May20 = flg.Period10,
+                        Jun20 = flg.Period11,
+                        Jul20 = flg.Period12,
+                        AugMar = flg.Period1To8,
+                        AprJul = flg.Period9To12,
+                        YearToDate = flg.YearToDate,
+                        Total = flg.Total
+                    }))).ToList();
+
+                Stopwatch stopWatchLog = new Stopwatch();
+                stopWatchLog.Start();
+                await _persistReportData.PersistReportDataAsync(
+                    persistModels,
+                    reportServiceContext.Ukprn,
+                    reportServiceContext.ReturnPeriod,
+                    TableNameConstants.FundingSummaryReport,
+                    reportServiceContext.ReportDataConnectionString,
+                    cancellationToken);
+                _logger.LogDebug($"Performance-FundingSummaryReport logging took - {stopWatchLog.ElapsedMilliseconds} ms ");
+                stopWatchLog.Stop();
+            }
+            else
+            {
+                _logger.LogDebug(" Data Persist Feature is disabled.");
             }
         }
     }
---
+++ src/ESFA.DC.PeriodEnd.ReportService.Service/Reports/FundingSummaryReport/Model/FundingSummaryPersistModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ESFA.DC.PeriodEnd.ReportService.Model.ReportModels;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Service.Reports.FundingSummaryReport.Model
+{
+    public class FundingSummaryPersistModel : AbstractReportModel
+    {
+        public int? Ukprn { get; set; }
+
+        public string ContractNo { get; set; }
+
+        public string FundingCategory { get; set; }
+
+        public string FundingSubCategory { get; set; }
+
+        public string FundLineGroup { get; set; }
+
+        public decimal Aug19 { get; set; }
+
+        public decimal Sep19 { get; set; }
+
+        public decimal Oct19 { get; set; }
+
+        public decimal Nov19 { get; set; }
+
+        public decimal Dec19 { get; set; }
+
+        public decimal Jan20 { get; set; }
+
+        public decimal Feb20 { get; set; }
+
+        public decimal Mar20 { get; set; }
+
+        public decimal Apr20 { get; set; }
+
+        public decimal May20 { get; set; }
+
+        public decimal Jun20 { get; set; }
+
+        public decimal Jul20 { get; set; }
+
+        public decimal AugMar { get; set; }
+
+        public decimal AprJul { get; set; }
+
+        public decimal YearToDate { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}

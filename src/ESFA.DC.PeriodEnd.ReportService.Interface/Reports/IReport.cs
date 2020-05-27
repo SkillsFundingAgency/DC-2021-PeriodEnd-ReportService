bf2951ca4726390638990079b344f2d3501f49a0
kelvin.morris <kelvin.morris@eas.co.uk>
2020-05-27 10:10:30 +0100

    #99507 WIP new interfaces

---
+++ src/ESFA.DC.PeriodEnd.ReportService.Interface/Reports/IReport.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Interface.Reports
+{
+    public interface IReport
+    {
+        string ReportTaskName { get; }
+
+        Task GenerateReport(IReportServiceContext reportServiceContext, CancellationToken cancellationToken);
+    }
+}

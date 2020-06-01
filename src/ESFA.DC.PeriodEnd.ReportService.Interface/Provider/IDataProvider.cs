227a40f2cd98aaffa93b14a474c0b27febb8c72a
kelvin.morris <kelvin.morris@eas.co.uk>
2020-06-01 09:46:22 +0100

    #99507 Adding new interfaces

---
+++ src/ESFA.DC.PeriodEnd.ReportService.Interface/Provider/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Interface.Provider
+{
+    public interface IDataProvider
+    {
+        Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken);
+    }
+}
--- src/ESFA.DC.PeriodEnd.ReportService.Interface/Reports/IReport.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Interface/Reports/IReport.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ESFA.DC.PeriodEnd.ReportService.Interface.Reports
@@ -6,6 +8,8 @@ namespace ESFA.DC.PeriodEnd.ReportService.Interface.Reports
     public interface IReport
     {
         string ReportTaskName { get; }
+
+        IEnumerable<Type> DependsOn { get; }
 
         Task GenerateReport(IReportServiceContext reportServiceContext, CancellationToken cancellationToken);
     }

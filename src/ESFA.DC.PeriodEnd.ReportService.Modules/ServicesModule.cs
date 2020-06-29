fd3b941ebd37ddea2bc74091e114edf6e920d992
Anil Pabbati <anil.pabbati@outlook.com>
2020-06-29 16:18:18 +0100

    Runtime errors resolved.

--- src/ESFA.DC.PeriodEnd.ReportService.Modules/ServicesModule.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Modules/ServicesModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using ESFA.DC.CsvService;
+using ESFA.DC.CsvService.Interface;
 using ESFA.DC.ExcelService;
 using ESFA.DC.ExcelService.Interface;
 using ESFA.DC.PeriodEnd.ReportService.Reports.Interface;
@@ -10,6 +12,7 @@ namespace ESFA.DC.PeriodEnd.ReportService.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<CsvFileService>().As<ICsvFileService>();
             builder.RegisterType<ExcelFileService>().As<IExcelFileService>();
             builder.RegisterType<FileNameService>().As<IFileNameService>();
         }

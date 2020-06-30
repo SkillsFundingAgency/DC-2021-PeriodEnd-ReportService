adfd66229a5ea54a683eec6223f61a277860abf0
Hari Dupati <dupatihari@gmail.com>
2020-06-30 09:25:35 +0100

    100495 - removed commented code

--- src/ESFA.DC.PeriodEnd.ReportService.Reports.Interface/AppsCoInvestment/Model/AppsCoInvestmentRecord.cs
+++ src/ESFA.DC.PeriodEnd.ReportService.Reports.Interface/AppsCoInvestment/Model/AppsCoInvestmentRecord.cs
@@ -25,26 +25,7 @@ namespace ESFA.DC.PeriodEnd.ReportService.Reports.Interface.AppsCoInvestment.Mod
 
         public string LDM356Or361 { get; set; }
 
-
-        //public decimal? TotalPMRPreviousFundingYears { get; set; }
-
-        //public decimal? TotalCoInvestmentDueFromEmployerInPreviousFundingYears { get; set; }
-
-        //public decimal? TotalPMRThisFundingYear { get; set; }
-
-        //public decimal? TotalCoInvestmentDueFromEmployerThisFundingYear { get; set; }
-
-        //public decimal PercentageOfCoInvestmentCollected { get; set; }
-
         public EarningsAndPayments EarningsAndPayments { get; set; }
-
-        //public CoInvestmentPaymentsDueFromEmployer CoInvestmentPaymentsDueFromEmployer { get; set; }
-
-        //public decimal CompletionEarningThisFundingYear { get; set; }
-
-        //public decimal CompletionPaymentsThisFundingYear { get; set; }
-
-        //public decimal? EmployerCoInvestmentPercentage { get; set; }
 
         public AECLearningDelivery AecLearningDelivery { get; set; }
     }

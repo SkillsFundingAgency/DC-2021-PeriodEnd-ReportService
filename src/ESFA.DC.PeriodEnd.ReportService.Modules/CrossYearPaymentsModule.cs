b654a89ada4640a12662e94000ecb2bd63a83019
kelvin.morris <kelvin.morris@eas.co.uk>
2020-09-11 09:21:04 +0100

    wip crossyearpayments report

---
+++ src/ESFA.DC.PeriodEnd.ReportService.Modules/CrossYearPaymentsModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using ESFA.DC.PeriodEnd.ReportService.Interface.Configuration;
+using ESFA.DC.PeriodEnd.ReportService.Reports.CrossYearPayments;
+using ESFA.DC.PeriodEnd.ReportService.Reports.Data.CrossYearPayments;
+using ESFA.DC.PeriodEnd.ReportService.Reports.Interface.CrossYearPayments;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Modules
+{
+    public class CrossYearPaymentsModule : AbstractReportModule<CrossYearPayments>
+    {
+        public CrossYearPaymentsModule(IDataPersistConfiguration dataPersistConfiguration) : base(dataPersistConfiguration)
+        {
+        }
+
+        protected override void RegisterDataProviders(ContainerBuilder builder)
+        {
+            builder.RegisterType<CrossYearPaymentsDataProvider>().As<ICrossYearDataProvider>();
+        }
+
+        protected override void RegisterModelBuilder(ContainerBuilder builder)
+        {
+            builder.RegisterType<CrossYearPaymentsModelBuilder>().As<ICrossYearModelBuilder>();
+        }
+
+        protected override void RegisterRenderService(ContainerBuilder builder)
+        {
+            builder.RegisterType<CrossYearPaymentsRenderService>().As<ICrossYearRenderService>();
+        }
+
+        protected override void RegisterServices(ContainerBuilder builder)
+        {
+        }
+
+        protected override void RegisterPersistenceService(ContainerBuilder builder)
+        {
+        }
+    }
+}
---
+++ src/ESFA.DC.PeriodEnd.ReportService.Reports/CrossYearPayments/CrossYearPaymentsModelBuilder.cs
@@ -0,0 +1,15 @@
+﻿using ESFA.DC.PeriodEnd.ReportService.Reports.Interface;
+using ESFA.DC.PeriodEnd.ReportService.Reports.Interface.CrossYearPayments;
+using ESFA.DC.PeriodEnd.ReportService.Reports.Interface.CrossYearPayments.Data;
+using ESFA.DC.PeriodEnd.ReportService.Reports.Interface.CrossYearPayments.Model;
+
+namespace ESFA.DC.PeriodEnd.ReportService.Reports.CrossYearPayments
+{
+    public class CrossYearPaymentsModelBuilder : ICrossYearModelBuilder
+    {
+        public CrossYearPaymentsModel Build(CrossYearDataModel dataModel, IReportServiceContext reportServiceContext)
+        {
+            return new CrossYearPaymentsModel();
+        }
+    }
+}
